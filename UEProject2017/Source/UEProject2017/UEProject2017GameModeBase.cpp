47e4d76d93d9c0313e77976e963628eee516a0b4
KevinKoek <nightarrowdevelopers@gmail.com>
2017-03-10 21:34:35 +0100

    Created: Unreal Engine project, Moved: .gitignore, Updated: Adding a line of code to ignore a big file in .gitignore

---
+++ UEProject2017/Source/UEProject2017/UEProject2017GameModeBase.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UEProject2017.h"
+#include "UEProject2017GameModeBase.h"
+
+
+
+
---
+++ UEProject2017/Source/UEProject2017/UEProject2017GameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "UEProject2017GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UEPROJECT2017_API AUEProject2017GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
